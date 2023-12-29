97135ef53a70d95f9d2386f363c2919f6fe2b99a
emiliano.caceres@aicore.com.ar <caceresmiranda05@gmail.com>
2023-12-29 12:26:21 -0300

    [FIX] change MenuMobile component

--- src/components/organisms/components/MenuMobile.tsx
+++ src/components/organisms/components/MenuMobile.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 
-import { PAGES, SERVICES } from '@/helpers/consts';
+import { CPAGES } from '@/helpers/consts';
 import { BurgerIcon } from '@/components/atoms/BurgerIcon';
 
 import styles from '../styles/nav-bar.module.css';
+
+import { ContainerNavCollapse } from './ContainerNavCollapse';
 
 export function MenuMobile() {
     const [isMenuOpen, setMenuOpen] = useState(false);
@@ -22,24 +24,14 @@ export function MenuMobile() {
                 <div className={styles._menu_overlay}>
                     <div className={styles._cancel_icon} onClick={handleBurgerClick} />
                     <ul className={styles._menu_links}>
-                        {PAGES.map((page) => (
+                        {CPAGES.map((page) => (
                             <li key={page.href}>
-                                {page.id === 2 ? (
-                                    <div>
-                                        <p className={styles._button_services}>Servicios</p>
-                                        <ul className={styles._ul_services}>
-                                            {SERVICES.map((service) => (
-                                                <li key={service.href} className={styles._li}>
-                                                    <a
-                                                        className={styles._link_services}
-                                                        href={service.href}
-                                                    >
-                                                        {service.text}
-                                                    </a>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </div>
+                                {page.isCollapse ? (
+                                    <ContainerNavCollapse
+                                        id={page.id}
+                                        items={page.items}
+                                        label={page.text}
+                                    />
                                 ) : (
                                     <a className={styles._link} href={page.href}>
                                         {page.text}
